bd9320939806e1e6593d5b753882a44f3a7c07f9
ra <transfluxus@posteo.de>
2023-05-12 10:40:23 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+function someCall() {
+    debugger
+    console.log("call")
+}
+
+export function start(){
+    document.body.appendChild(document.createTextNode("Hello world!!!!!"))
+}
+
+start()
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        watchFiles: ['src/**/*'],
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './index.html'), // path to your index.html file
+        }),
+    ],
+};
